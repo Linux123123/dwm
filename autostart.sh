00c6e95927d961be245432cfa6c409f6029419bc
Linux123123 <me@linux123123.com>
2022-07-06 21:05:01 +0300

    Sync

--- autostart.sh
+++ autostart.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
+numlockx on &
 nitrogen --restore &
 dwmblocks &
 nm-applet &
-thunar --daemon &
-numlockx on &
-polkit-dumb-agent & 
-dunst &
-copyq &
-flameshot &
-/home/linux123123/.local/bin/hyperion-x11 -a 192.168.0.131:19400 -s 25 &
+polkit-dumb-agent &
 discord &
 youtube-music &
 blueman-applet &
+/home/linux123123/.local/bin/hyperion-x11 -a 192.168.0.131:19400 -s 25 &
 nextcloud --background &
-mailspring -b &
+dunst &
+copyq &
+flameshot &
+thunar --daemon &
+birdtray &
 
 eval $(gnome-keyring-daemon --start)
 export SSH_AUTH_SOCK
